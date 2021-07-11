14641f09ad95420ba3d60351dd5c8d55b5deb2a1
Harshks2406 <harshsingh.hks@gmail.com>
2021-07-11 13:17:04 +0530

    Handeled All posts

--- Social-media-App/src/public/app/common.js
+++ Social-media-App/src/public/app/common.js
@@ -1,6 +1,7 @@
 $(()=>{
     $('#navbar').load('components/navbar.html',loginIfNeeded)
     $('#footer').load('components/footer.html')
+    $('#content').load('components/all-posts.html',loadPosts)
 
 })
 
@@ -19,4 +20,29 @@ function loginIfNeeded(){
         console.log('Resuming Session as:',currentUser.username)
         $('#nav-username').text(currentUser.username)
     }
+}
+
+function loadPosts(){
+    $.get('/api/posts', (posts)=>{
+        for (let p of posts){
+            $('#post-container').append(
+                $(`
+                <div class="col-4">
+                    <div class="card " style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${p.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                            <p class="card-text">
+                                ${p.body.substr(0,200)}
+                                <a href="#" class="card-link">...read more</a>
+                            </p>
+                            <a href="#" class="card-link">Comment</a>
+                            <a href="#" class="card-link">Like</a>
+                        </div>
+                    </div>
+                </div>
+                `)
+            )
+        }
+    })
 }
